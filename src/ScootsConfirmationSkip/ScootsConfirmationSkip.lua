b94389d52ce692ac1e7fb53dcfb90495624e88bd
SynScoots <admin@winterwind.co.uk>
2025-03-13 10:06:27 +0000

    Suppress confirmation skip when auction house is open

--- src/ScootsConfirmationSkip/ScootsConfirmationSkip.lua
+++ src/ScootsConfirmationSkip/ScootsConfirmationSkip.lua
@@ -1,10 +1,17 @@
 SCK = {}
 SCK.confirmActive = false
+SCK.auctionsOpen = false
 
 SCK.frame = CreateFrame('Frame', nil, UIParent)
 
 function SCK.handleEvent(self, event)
-	SCK.confirmActive = true
+	if(event == 'AUCTION_HOUSE_SHOW') then
+		SCK.auctionsOpen = true
+	elseif(event == 'AUCTION_HOUSE_CLOSED') then
+		SCK.auctionsOpen = false
+	elseif(SCK.auctionsOpen == false) then
+		SCK.confirmActive = true
+	end
 end
 
 function SCK.loop(self, event)
@@ -22,4 +29,6 @@ SCK.frame:SetScript('OnEvent', SCK.handleEvent)
 SCK.frame:RegisterEvent('EQUIP_BIND_CONFIRM')
 SCK.frame:RegisterEvent('AUTOEQUIP_BIND_CONFIRM')
 SCK.frame:RegisterEvent('LOOT_BIND_CONFIRM')
+SCK.frame:RegisterEvent('AUCTION_HOUSE_SHOW')
+SCK.frame:RegisterEvent('AUCTION_HOUSE_CLOSED')
 --SCK.frame:RegisterEvent('DELETE_ITEM_CONFIRM')
