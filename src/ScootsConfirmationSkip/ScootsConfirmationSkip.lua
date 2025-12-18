c8f49ebf08d56e2fd7087b55beea6a93c1c22bc3
SynScoots <admin@winterwind.co.uk>
2025-12-18 12:51:29 +0000

    Added group need/greed/disenchant

--- src/ScootsConfirmationSkip/ScootsConfirmationSkip.lua
+++ src/ScootsConfirmationSkip/ScootsConfirmationSkip.lua
@@ -1,5 +1,5 @@
 ScootsConfirmationSkip = {
-    ['version'] = '2.0.0',
+    ['version'] = '2.1.0',
     ['frame'] = CreateFrame('Frame', 'ScootsConfirmationSkip-EventsFrame', UIParent),
     ['activeEvents'] = {},
     ['eventMap'] = {
@@ -15,14 +15,23 @@ ScootsConfirmationSkip = {
             ['popupWhich'] = 'LOOT_BIND',
             ['functionName'] = 'ConfirmLootSlot',
         },
+        ['CONFIRM_LOOT_ROLL'] = {
+            ['popupWhich'] = 'CONFIRM_LOOT_ROLL',
+            ['functionName'] = 'ConfirmLootRoll',
+        },
+        ['CONFIRM_DISENCHANT_ROLL'] = {
+            ['popupWhich'] = 'CONFIRM_LOOT_ROLL',
+            ['functionName'] = 'ConfirmLootRoll',
+        },
         ['DELETE_ITEM_CONFIRM'] = {
             ['popupWhich'] = 'DELETE_ITEM',
             ['functionName'] = 'DeleteCursorItem',
         },
     },
+    ['eventActions'] = {},
 }
 
-ScootsConfirmationSkip.handleEvent = function(self, event)
+ScootsConfirmationSkip.handleEvent = function(self, event, ...)
     local suppress = false
 
     if(_G['AuctionFrame'] ~= nil and _G['AuctionFrame']:IsVisible()) then
@@ -34,6 +43,15 @@ ScootsConfirmationSkip.handleEvent = function(self, event)
     if(suppress == true) then
         ScootsConfirmationSkip.activeEvents = {}
     elseif(ScootsConfirmationSkip.eventMap[event] ~= nil) then
+        if(event == 'CONFIRM_LOOT_ROLL' or event == 'CONFIRM_DISENCHANT_ROLL') then
+            local rollId = select(1, ...)
+            local roll = select(2, ...)
+            
+            ScootsConfirmationSkip.eventActions[event] = function()
+                _G[ScootsConfirmationSkip.eventMap[event]['functionName']](rollId, roll)
+            end
+        end
+        
         table.insert(ScootsConfirmationSkip.activeEvents, event)
     end
 end
@@ -51,7 +69,13 @@ ScootsConfirmationSkip.loop = function()
                         local popup = _G['StaticPopup' .. popupIndex]
                         
                         if(popup and popup:IsVisible() and popup.which == ScootsConfirmationSkip.eventMap[event].popupWhich) then
-                            _G[ScootsConfirmationSkip.eventMap[event].functionName](popup.data)
+                            if(ScootsConfirmationSkip.eventActions[event] ~= nil) then
+                                ScootsConfirmationSkip.eventActions[event]()
+                                ScootsConfirmationSkip.eventActions[event] = nil
+                            else
+                                _G[ScootsConfirmationSkip.eventMap[event].functionName](popup.data)
+                            end
+                            
                             StaticPopup_Hide(popup.which)
                             doLoop = false
                             
@@ -74,4 +98,6 @@ ScootsConfirmationSkip.frame:SetScript('OnUpdate', ScootsConfirmationSkip.loop)
 ScootsConfirmationSkip.frame:RegisterEvent('EQUIP_BIND_CONFIRM')
 ScootsConfirmationSkip.frame:RegisterEvent('AUTOEQUIP_BIND_CONFIRM')
 ScootsConfirmationSkip.frame:RegisterEvent('LOOT_BIND_CONFIRM')
+ScootsConfirmationSkip.frame:RegisterEvent('CONFIRM_LOOT_ROLL')
+ScootsConfirmationSkip.frame:RegisterEvent('CONFIRM_DISENCHANT_ROLL')
 --ScootsConfirmationSkip.frame:RegisterEvent('DELETE_ITEM_CONFIRM')
